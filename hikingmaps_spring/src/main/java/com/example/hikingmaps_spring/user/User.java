3eeef98ac84adc2903c5b075b565a6d3fa3b8ac7
abrax223 <mar-jak1937@wp.pl>
2018-10-27 17:19:08 +0200

    email address field added

--- hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/user/User.java
+++ hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/user/User.java
@@ -11,29 +11,30 @@ public class User {
 	private long id;
 	private String login;
 	private String password;
+	private String email;
 	public User()
 	{
-		
 	}
 	public User(String login, String password) {
 		this.login = login;
 		this.password = password;
 	}
-
 	public long getId() {
 		return id;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 	public String getLogin() {
 		return login;
 	}
-	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
