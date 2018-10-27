eca056a16d5270af40f6ac69d0f8979e6e440924
abrax223 <mar-jak1937@wp.pl>
2018-10-27 17:20:34 +0200

    UserService

---
+++ hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/user/UserService.java
@@ -0,0 +1,15 @@
+package com.example.hikingmaps_spring.user;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.example.hikingmaps_spring.user.exceptions.*;
+@Configuration
+public class UserService {
+	public void register(User user) throws PasswordTooShortException, UserExistsException, InvalidEmailException
+	{	
+		if(user.getPassword().length()<8||user.getPassword().length()>30)
+			throw new PasswordTooShortException();
+		//TODO check for user existence
+		//TODO email address validation
+	}
+}
