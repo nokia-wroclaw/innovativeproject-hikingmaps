2329d6e9a578b6de569ab4b9632c2e69013bbccc
jarek <jarek@vaio.home>
2018-11-01 15:27:35 +0100

    User service now communicates with server

--- hikingmaps-angular/src/app/user.service.ts
+++ hikingmaps-angular/src/app/user.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) {}
 
   public addUser(username: string, email: string, password: string) {
-
+    return this.http.post(`${environment.apiUrl}/user/register`, {username, email, password});
   }
 }
