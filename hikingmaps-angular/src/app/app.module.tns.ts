ab679d1f5f877c1476353c092bbc9086e5e2c9c1
abrax223 <mar-jak1937@wp.pl>
2018-11-02 18:29:19 +0100

    another conflict markers...

--- hikingmaps-angular/src/app/app.module.tns.ts
+++ hikingmaps-angular/src/app/app.module.tns.ts
@@ -6,22 +6,16 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
 import { NativeScriptHttpClientModule } from 'nativescript-angular/http-client';
 import { NativeScriptFormsModule } from 'nativescript-angular/forms';
-<<<<<<< HEAD
 import { RegisterComponent } from './register/register.component';
-=======
 import {MapsComponent} from './maps/maps.component';
->>>>>>> d5924416e6f6d6cf72d6ec8bea695a41e923b0ba
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloComponent,
-<<<<<<< HEAD
     RegisterComponent,
-=======
     MapsComponent
->>>>>>> d5924416e6f6d6cf72d6ec8bea695a41e923b0ba
   ],
   imports: [
     NativeScriptModule,
