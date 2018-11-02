051db4bbffc37ce48119dfc25ed0e0abc53d9ff2
abrax223 <mar-jak1937@wp.pl>
2018-11-02 18:12:25 +0100

    conflict encountered... solved? honestly i have no idea, but the map works, so...

--- hikingmaps-angular/src/app/app.module.ts
+++ hikingmaps-angular/src/app/app.module.ts
@@ -5,21 +5,15 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
-<<<<<<< HEAD
 import { RegisterComponent } from './register/register.component';
-=======
 import {MapsComponent} from './maps/maps.component';
->>>>>>> d5924416e6f6d6cf72d6ec8bea695a41e923b0ba
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloComponent,
-<<<<<<< HEAD
     RegisterComponent,
-=======
     MapsComponent
->>>>>>> d5924416e6f6d6cf72d6ec8bea695a41e923b0ba
   ],
   imports: [
     BrowserModule,
