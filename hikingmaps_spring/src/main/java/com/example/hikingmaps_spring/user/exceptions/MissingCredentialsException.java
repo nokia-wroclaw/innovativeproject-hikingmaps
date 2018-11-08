1afe8ea8b41c79be3e28c91f3308f2568012788c
abrax223 <mar-jak1937@wp.pl>
2018-11-08 18:23:12 +0100

    exception thrown when any of the required credentials - email, password, login -  is missing

---
+++ hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/user/exceptions/MissingCredentialsException.java
@@ -0,0 +1,12 @@
+package com.example.hikingmaps_spring.user.exceptions;
+
+public class MissingCredentialsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2301760419886285638L;
+
+	
+
+}
