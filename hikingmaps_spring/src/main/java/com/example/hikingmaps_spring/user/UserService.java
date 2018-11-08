9d27d5665fc925c69cc190bfcb4ef415d6ed5ad6
abrax223 <mar-jak1937@wp.pl>
2018-11-08 19:09:37 +0100

    sort of a primitive registration process implemented

--- hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/user/UserService.java
+++ hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/user/UserService.java
@@ -1,15 +1,32 @@
 package com.example.hikingmaps_spring.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.TransactionSystemException;
 
 import com.example.hikingmaps_spring.user.exceptions.*;
 @Configuration
 public class UserService {
-	public void register(User user) throws PasswordTooShortException, UserExistsException, InvalidEmailException
-	{	
+	private final UserRepository repository;
+	@Autowired
+	public UserService(UserRepository repository)
+	{	this.repository=repository;
+	}
+	public void register(User user) throws PasswordTooShortException, UserExistsException, InvalidEmailException, MissingCredentialsException
+	{	if( user.getPassword()==null
+			 ||user.getLogin()==null
+			 ||user.getEmail()==null)
+			throw new MissingCredentialsException();
 		if(user.getPassword().length()<8)
 			throw new PasswordTooShortException();
-		//TODO check for user existence
-		//TODO email address validation
+		for(User u: repository.findAll())
+			if(u.getLogin().equals(user.getLogin()))
+				throw new UserExistsException();
+		try
+		{	this.repository.save(user);
+		}
+		catch(TransactionSystemException e)
+		{	throw new InvalidEmailException();
+		}
 	}
 }
