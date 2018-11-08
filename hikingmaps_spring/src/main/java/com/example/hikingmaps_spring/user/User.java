5aaebf90b580a405a0d9ef36c9c3512aa1078874
abrax223 <mar-jak1937@wp.pl>
2018-11-08 18:59:33 +0100

    email constraint added

--- hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/user/User.java
+++ hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/user/User.java
@@ -3,6 +3,7 @@ package com.example.hikingmaps_spring.user;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
 
 @Entity
 public class User {
@@ -11,6 +12,7 @@ public class User {
 	private long id;
 	private String login;
 	private String password;
+	@Email
 	private String email;
 	public User()
 	{
