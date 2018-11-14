bdb31af01540a8dffd7a6413ae09fdfb49fc8420
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2018-11-14 16:10:13 +0100

    Registration validation

--- hikingmaps-angular/src/app/register/register.component.ts
+++ hikingmaps-angular/src/app/register/register.component.ts
@@ -10,8 +10,14 @@ import { MessageService } from 'primeng/api';
 export class RegisterComponent implements OnInit {
 
   public username = '';
+  public goodUsername = true;
   public email = '';
+  public goodEmail = true;
   public password = '';
+  public goodPassword = true;
+
+  // tslint:disable-next-line:max-line-length
+  private emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   constructor(
     private userService: UserService,
@@ -22,6 +28,16 @@ export class RegisterComponent implements OnInit {
   }
 
   handleSubmit() {
+    if (!this.goodUsername || this.username === '') {
+      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Empty username'});
+      return;
+    } else if (!this.goodEmail || this.email === '') {
+      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Bad email' });
+      return;
+    } else if (!this.goodPassword || this.password === '') {
+      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Password too short' });
+      return;
+    }
     this.userService.addUser(this.username, this.email, this.password)
       .subscribe(() => {
         // send message about succes and reroute
@@ -31,6 +47,19 @@ export class RegisterComponent implements OnInit {
         this.messageService.add({ severity: 'error', summary: 'Error',
           detail: (error.error.message) ? error.error.message : error.statusText });
       });
+  }
+
+  valdateLogin() {
+    console.log(this.goodUsername);
+    this.goodUsername = this.username.length > 0;
+  }
+
+  valdateEmail() {
+    this.goodEmail = this.emailRegex.test(this.email);
+  }
+
+  valdatePassword() {
+    this.goodPassword = this.password.length >= 8;
   }
 
 }
