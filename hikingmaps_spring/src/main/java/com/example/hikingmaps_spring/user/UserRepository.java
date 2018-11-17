4fb83ae42b9f115c73a545a2c38edf570720b071
abrax223 <mar-jak1937@wp.pl>
2018-11-17 09:09:58 +0100

    a method has been added

--- hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/user/UserRepository.java
+++ hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/user/UserRepository.java
@@ -2,6 +2,6 @@ package com.example.hikingmaps_spring.user;
 
 import org.springframework.data.repository.CrudRepository;
 
-
 public interface UserRepository extends CrudRepository<User, Long> {
+	User findByLogin(String login);
 }
