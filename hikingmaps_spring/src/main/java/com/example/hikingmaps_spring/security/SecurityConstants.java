9b24fcc7561315fdff8cfa5ee4a7fad6b585f7e1
abrax223 <mar-jak1937@wp.pl>
2018-11-23 14:46:38 +0100

    redundant constants removed

--- hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/security/SecurityConstants.java
+++ hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/security/SecurityConstants.java
@@ -3,10 +3,7 @@ package com.example.hikingmaps_spring.security;
 public class SecurityConstants {
 	public static final String SECRET = "HikingMapsJWTGenerationSecretKey";
 	public static final String TOKEN_PREFIX = "Bearer ";
-	public static final long EXPIRATION_TIME = 86_400_000; // 1 day
-	public static final String HEADER = "Authorization";
+	public static final long TOKEN_EXPIRATION_TIME = 86_400_000; // 1 day
+	public static final String AUTHORIZATION_HEADER = "Authorization";
 	public static final String SIGN_IN_URL = "/user/login";
-	public static final String SIGN_UP_URL = "/user/register";
-	public static final String H2_CONSOLE = "/h2-console/**";
-	public static final String SWAGGER_UI = "/swagger-ui.html";
 }
