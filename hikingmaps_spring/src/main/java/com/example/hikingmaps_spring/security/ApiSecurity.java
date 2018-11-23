1fa6595f8645a832253708da502e5fd88a84f71b
abrax223 <mar-jak1937@wp.pl>
2018-11-23 14:45:14 +0100

    api security refactored(once again)

--- hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/security/ApiSecurity.java
+++ hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/security/ApiSecurity.java
@@ -1,6 +1,5 @@
 package com.example.hikingmaps_spring.security;
 
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,31 +9,31 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.example.hikingmaps_spring.user.UserRepository;
+
 import org.springframework.context.annotation.Bean;
 
 @EnableWebSecurity
 public class ApiSecurity extends WebSecurityConfigurerAdapter {
 	private UserDetailsServiceImplementation userDetailsService;
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	private UserRepository repository;
 
-	public ApiSecurity(UserDetailsServiceImplementation userDetailsService,
+	public ApiSecurity(UserRepository repository, UserDetailsServiceImplementation userDetailsService,
 			BCryptPasswordEncoder bCryptPasswordEncoder) {
+		this.repository = repository;
 		this.userDetailsService = userDetailsService;
 		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().csrf().disable().authorizeRequests()
-				.antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
-				.antMatchers(SecurityConstants.H2_CONSOLE).permitAll()
-				.antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security",
-						"/swagger-ui.html", "/webjars/**", "/swagger-resources/configuration/ui")
-				.permitAll().anyRequest().authenticated().and()
-				.addFilter(new JWTAuthenticationFilter(authenticationManager()))
-				.addFilter(new JWTAuthorizationFilter(authenticationManager())).sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.headers().frameOptions().disable();
+		http.cors().and().csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and().authorizeRequests().antMatchers("/sec/**").hasAuthority("user").and().authorizeRequests()
+				.antMatchers("/admin/**").hasAuthority("admin").and()
+				.addFilter(new JWTAuthenticationFilter(repository, authenticationManager()))
+				.addFilter(new JWTAuthorizationFilter(authenticationManager())).headers().frameOptions().disable();
 	}
 
 	@Override
