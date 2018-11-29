af213a09ec61723c0e2de42e3e82f1214dd536e3
Jarosław Pokropiński <34548796+JaroslawPokropinski@users.noreply.github.com>
2018-11-29 21:30:56 +0000

    Revert "Routes backend (#33)" (#34)
    This reverts commit 68c410ebcde930dbebf8e3c669cb4cf2addbacbf.

--- hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/route/RouteRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.hikingmaps_spring.route;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RouteRepository extends CrudRepository<Route, Long> {
-}
--- hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/route/exceptions/AdminPermissionsRequiredException.java
+++
@@ -1,9 +0,0 @@
-package com.example.hikingmaps_spring.route.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Permission denied (admin only)")
-public class AdminPermissionsRequiredException extends RuntimeException {
-    private static final long serialVersionUID = -3985550153609048149L;
-}
