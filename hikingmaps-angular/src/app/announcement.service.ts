660c1b4d64c2c66483b7da66a3a7fd0fc5010f51
adolzycki <a.dolzycki@gmail.com>
2018-11-29 17:36:18 +0100

    Bracket fix

--- hikingmaps-angular/src/app/announcement.service.ts
+++ hikingmaps-angular/src/app/announcement.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Announcement } from 'src/app/announcement';
 import { environment } from '../environments/environment';
 import {SessionService} from './session.service';
@@ -9,7 +9,6 @@ import {SessionService} from './session.service';
 })
 export class AnnouncementService {
 
-
   constructor(
     private http: HttpClient,
     private auth: SessionService
@@ -17,18 +16,15 @@ export class AnnouncementService {
 
   getAnnouncements()  {
     const key = this.auth.getKey();
-
-    return this.http.get<Announcement[]>(`${environment.apiUrl}/sec/ann/all`, { headers: {'authorization': key}});
+    return this.http.get<Announcement[]>(`${environment.apiUrl}/sec/ann/all`, {headers: {'authorization': key}});
   }
   public addAnnouncement( title: string,  start: string, destination: string, description: string, date: string) {
     const key = this.auth.getKey();
-    return this.http.post(`${environment.apiUrl}/sec/ann/add`, { title, date, description, start, destination}, { observe: 'response', headers: {'authorization': key} });
+    return this.http.post(`${environment.apiUrl}/sec/ann/add`, { title, date, description, start, destination}, {headers: {'authorization': key}});
   }
   addInterest(id: String) {
     const key = this.auth.getKey();
-    console.log(id);
-    console.log(key);
-    return this.http.post(`${environment.apiUrl}/sec/ann/interest`,  {params: {annId: id},  observe: 'response', headers: {'authorization': key} });
+    return this.http.post(`${environment.apiUrl}/sec/ann/interest?annId=${id}`, {headers: {'authorization': key}});
   }
-  }
+
 }
