2651235ec643bd5c3d28267f322471e35207a323
abrax223 <mar-jak1937@wp.pl>
2018-12-01 13:38:34 +0100

    find by owner method added

--- hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/announcement/AnnouncementRepository.java
+++ hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/announcement/AnnouncementRepository.java
@@ -1,7 +1,12 @@
 package com.example.hikingmaps_spring.announcement;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
+import com.example.hikingmaps_spring.user.User;
+
 public interface AnnouncementRepository extends CrudRepository<Announcement, Long> {
+	public List<Announcement> findByOwner(User user);
 
 }
