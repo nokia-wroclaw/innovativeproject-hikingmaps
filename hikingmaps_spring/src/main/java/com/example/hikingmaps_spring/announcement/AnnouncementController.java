99bf88bdbfeca637373b8a9d74b74d59c7d71e45
abrax223 <mar-jak1937@wp.pl>
2018-12-01 10:01:07 +0100

    announcement can be edited and deleted

--- hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/announcement/AnnouncementController.java
+++ hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/announcement/AnnouncementController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +28,18 @@ public class AnnouncementController {
 	@GetMapping("/all")
 	public ResponseEntity<List<Announcement>> getAll() {
 		return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
+	}
+	
+	@PatchMapping("/edit")
+	public ResponseEntity<Void> edit(Authentication authentication, @RequestBody Announcement announcement) {
+		service.edit(authentication.getName(), announcement);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/delete")
+	public ResponseEntity<Void> delete(Authentication authentication, @RequestParam long annId) {
+		service.delete(authentication.getName(), annId);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 	
 	@PostMapping("/add")
--- hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/announcement/AnnouncementService.java
+++ hikingmaps_spring/src/main/java/com/example/hikingmaps_spring/announcement/AnnouncementService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.hikingmaps_spring.announcement.exceptions.AnnouncementDoesnExistException;
+import com.example.hikingmaps_spring.announcement.exceptions.AnnouncementOwnerMismatchException;
 import com.example.hikingmaps_spring.user.User;
 import com.example.hikingmaps_spring.user.UserService;
 import com.example.hikingmaps_spring.user.exceptions.UserDoesntExistException;
@@ -16,7 +17,7 @@ import com.google.common.collect.Lists;
 public class AnnouncementService {
 	private AnnouncementRepository repository;
 	private UserService userService;
-	
+
 	@Autowired
 	public AnnouncementService(AnnouncementRepository repository, UserService userService) {
 		this.repository = repository;
@@ -32,13 +33,35 @@ public class AnnouncementService {
 		announcement.setOwner(user);
 		repository.save(announcement);
 	}
-	
+
+	public void edit(String username, Announcement annToEdit) {
+		Optional<User> optUser = userService.getByLogin(username);
+		User user = optUser.orElseThrow(UserDoesntExistException::new);
+		Optional<Announcement> optAnn = repository.findById(annToEdit.getId());
+		Announcement ann = optAnn.orElseThrow(AnnouncementDoesnExistException::new);
+		if (user.getId() != ann.getOwner().getId())
+			throw new AnnouncementOwnerMismatchException();
+		annToEdit.setOwner(ann.getOwner());
+		repository.save(annToEdit);
+	}
+
+	public void delete(String username, long annId) {
+		Optional<User> optUser = userService.getByLogin(username);
+		User user = optUser.orElseThrow(UserDoesntExistException::new);
+		Optional<Announcement> optAnn = repository.findById(annId);
+		Announcement ann = optAnn.orElseThrow(AnnouncementDoesnExistException::new);
+		if (user.getId() != ann.getOwner().getId())
+			throw new AnnouncementOwnerMismatchException();
+		repository.delete(ann);
+
+	}
+
 	public void interest(String username, long annId) {
 		Optional<User> optUser = userService.getByLogin(username);
 		User user = optUser.orElseThrow(UserDoesntExistException::new);
 		addUser(user, annId);
 	}
-	
+
 	public void addUser(User user, long annId) {
 		Announcement ann = repository.findById(annId).orElseThrow(AnnouncementDoesnExistException::new);
 		ann.getInterested().add(user);
