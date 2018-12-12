b0a9e48b61c4f728ae2a1ca7bda6cacd3508870c
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2018-12-12 01:25:20 +0100

    Changed displayed timeformat to year/month/day hour:minute

--- hikingmaps-angular/src/app/browse-announcement/browse-announcement.component.ts
+++ hikingmaps-angular/src/app/browse-announcement/browse-announcement.component.ts
@@ -37,9 +37,17 @@ export class BrowseAnnouncementComponent implements OnInit {
 
   ngOnInit() {
     this.announcementService.getAnnouncements()
-      .subscribe(
-        data => this.announcements = data
-      );
+      .subscribe( data => {
+        this.announcements = data;
+        const re = /(\d+)\-(\d+)\-(\d+)T(\d+):(\d+):(\d+)/;
+        for (let i = 0; i < this.announcements.length; i++) {
+          const m = this.announcements[i].date.match(re);
+          if (m) {
+            const h = (parseInt(m[4], 10) + 1) % 24; // add 1 cause poland is in such timezone
+            this.announcements[i].date = `${m[1]}/${m[2]}/${m[3]} ${h}:${m[5]}`;
+          }
+        }
+      });
     this.items = [
       {
         label: 'User',
