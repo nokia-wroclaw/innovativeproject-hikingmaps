18e901d8e507c6fbd626f7c0a99c2705ebd6114d
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2019-01-17 19:39:20 +0100

    last changes

---
+++ hikingmaps-angular/src/app/edit-routes/edit-routes.component.tns.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-routes',
+  templateUrl: './edit-routes.component.html',
+  styleUrls: ['./edit-routes.component.css']
+})
+export class EditRoutesComponent implements OnInit {
+  ngOnInit() {
+  }
+}
---
+++ hikingmaps-angular/src/app/route/route.component.tns.ts
@@ -0,0 +1,10 @@
+import { OnInit, Component } from '@angular/core';
+@Component({
+  selector: 'app-add-route',
+  templateUrl: './route.component.html',
+  styleUrls: ['./route.component.css']
+})
+export class RouteComponent implements OnInit {
+  ngOnInit() {
+  }
+}
